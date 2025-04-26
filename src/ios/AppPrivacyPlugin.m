e97f8d2c8904117b124bd7aa03e273cd28dba8af
Соловей Владислав Романович <uladzislau.salavei@softclub.by>
2025-04-26 12:41:30 +0300

    refactor: remove screenshots and screen record iOS code, because it doesn't work

--- src/ios/AppPrivacyPlugin.m
+++ src/ios/AppPrivacyPlugin.m
@@ -16,10 +16,6 @@
     dispatch_async(dispatch_get_main_queue(), ^{
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onAppWillResignActive:) name:UIApplicationWillResignActiveNotification object:nil];
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onAppDidBecomeActive:) name:UIApplicationDidBecomeActiveNotification object:nil];
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(userDidTakeScreenshot:) name:UIApplicationUserDidTakeScreenshotNotification object:nil];
-        if (@available(iOS 11.0, *)) {
-            [[UIScreen mainScreen] addObserver:self forKeyPath:@"captured" options:NSKeyValueObservingOptionNew context:nil];
-        }
     });
     self.privacyModeEnabled = NO;
 }
@@ -66,45 +62,6 @@
         self.privacyWindow.hidden = NO;
     });
 }
-- (void)userDidTakeScreenshot:(NSNotification *)notification {
-    if (!self.privacyModeEnabled) {
-        return; // Если режим приватности выключен — ничего не делаем
-    }
-    
-    NSLog(@"[AppPrivacyPlugin] Screenshot detected!");
-    
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [self applyPrivacyScreen];
-        
-        // Скрываем overlay через 2 секунды
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            [self removePrivacyScreen];
-        });
-    });
-}
-
-// Обработка изменения статуса записи экрана
-- (void)observeValueForKeyPath:(NSString *)keyPath
-                      ofObject:(id)object
-                        change:(NSDictionary<NSKeyValueChangeKey,id> *)change
-                       context:(void *)context {
-    if ([keyPath isEqualToString:@"captured"]) {
-        BOOL isCaptured = [change[NSKeyValueChangeNewKey] boolValue];
-        NSLog(@"[AppPrivacyPlugin] Screen recording status changed: %@", isCaptured ? @"Started" : @"Stopped");
-        
-        if (!self.privacyModeEnabled) {
-            return; // Если режим приватности выключен — не реагируем
-        }
-        
-        dispatch_async(dispatch_get_main_queue(), ^{
-            if (isCaptured) {
-                [self applyPrivacyScreen];
-            } else {
-                [self removePrivacyScreen];
-            }
-        });
-    }
-}
 
 - (void)removePrivacyScreen {
     dispatch_async(dispatch_get_main_queue(), ^{
@@ -117,9 +74,6 @@
 
 - (void)dealloc {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
-    if (@available(iOS 11.0, *)) {
-        [[UIScreen mainScreen] removeObserver:self forKeyPath:@"captured"];
-    }
 }
 
 @end
